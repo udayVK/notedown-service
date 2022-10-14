7f6a29a60cced34582c1b16beed81c6e2d1c6fb7
Uday. V Krishna <vamshi8724@.gmail.com>
2022-10-14 14:37:17 +0530

    modified the pojos

--- src/main/java/vm/money/track/pojo/Spend.java
+++ src/main/java/vm/money/track/pojo/Spend.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import lombok.Data;
 import lombok.AllArgsConstructor;
@@ -20,6 +21,7 @@ public class Spend {
     private int id;
 	private String heading;
     private LocalDate date;
+    @OneToMany
     private Map<Short, SubSpend> spends;
     
     
--- src/main/java/vm/money/track/repos/Repos.java
+++ src/main/java/vm/money/track/repos/Repos.java
@@ -8,9 +8,6 @@ import java.util.List;
 import vm.money.track.pojo.Spend;
 
 public interface Repos extends JpaRepository<Spend,Integer>{
-
-	//uses Query dsl to generate query
-	List<Spend> findByPurpose(String purpose);
 	
 	@Query(value = "delete from spend where data < ?1 ",nativeQuery=true)
 	void deleteOld(LocalDate ld);
@@ -19,7 +16,7 @@ public interface Repos extends JpaRepository<Spend,Integer>{
 	@Query(value="select * from spend where date>=?1 and date <?2",nativeQuery=true)
 	List<Spend> getBYMonth(LocalDate firstDayOfMonth, LocalDate FirstDayOfNextMonth);
 
-	@Query(value="select sum(money) from spend where date>=?1 and date <?2",nativeQuery = true)
+	@Query(value="select sum(money) from spend where date>=?1 and date < ?2",nativeQuery = true)
 	int monthlySpent(LocalDate firstDayOfMonth, LocalDate FirstDayOfNextMonth);
     
     
